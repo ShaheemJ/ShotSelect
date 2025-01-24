ec7cb6e8caa2a0b8ffc7ca292036e466c94d855d
Shaheem Jaleel <shaheem2006jaleel@gmail.com>
2025-01-24 12:22:33 -0600

    fixed connections

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -4,7 +4,6 @@ const nextConfig = {
       NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
     },
     webpack(config) {
-      console.log('API_BASE_URL in webpack config:', process.env.NEXT_PUBLIC_API_BASE_URL); // Log the variable
       return config;
     },
   };
