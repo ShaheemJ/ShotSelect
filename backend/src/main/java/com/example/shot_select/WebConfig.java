80e21cf91a96fe6c6f940c96e80b2a9ba47efe08
Shaheem Jaleel <shaheem2006jaleel@gmail.com>
2025-01-24 01:05:20 -0600

    reconfigured CORS

--- backend/src/main/java/com/example/shot_select/WebConfig.java
+++ backend/src/main/java/com/example/shot_select/WebConfig.java
@@ -1,6 +1,5 @@
 package com.example.shot_select;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -11,8 +10,9 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "https://shot-select.vercel.app") // Allow  frontend's URL
-                .allowedMethods("GET", "POST", "PUT", "DELETE") // Specify allowed HTTP methods
-                .allowedHeaders("*"); // Allow all headers
+                .allowedOrigins("http://localhost:3000", "https://shot-select.vercel.app") // Allow frontend's URL
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Specify allowed HTTP methods
+                .allowedHeaders("*") // Allow all headers
+                .allowCredentials(true); // Enable credentials if needed (e.g., for cookies/auth tokens)
     }
 }
