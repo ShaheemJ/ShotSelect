4a3a1a83b4b70bd68767f683a952ef26156e39ea
Shaheem Jaleel <shaheem2006jaleel@gmail.com>
2025-01-23 18:53:42 -0600

    url

--- frontend/utils/api.ts
+++ frontend/utils/api.ts
@@ -1,4 +1,4 @@
-const API_BASE_URL = (process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:8080').replace(/\/$/, '');
+const API_BASE_URL = (process.env.NEXT_PUBLIC_API_BASE_URL || '').replace(/\/$/, '');
 
 export default API_BASE_URL;
 
