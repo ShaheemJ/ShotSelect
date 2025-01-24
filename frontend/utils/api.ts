2558b1e0d3860a4ddd77d8a07e2acea9d76f220f
Shaheem Jaleel <shaheem2006jaleel@gmail.com>
2025-01-23 18:39:25 -0600

    url fix

--- frontend/utils/api.ts
+++ frontend/utils/api.ts
@@ -1,6 +1,4 @@
-//const API_BASE_URL = 'https://shotselect.onrender.com/';
-
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:8080';
+const API_BASE_URL = (process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:8080').replace(/\/$/, '');
 
 export default API_BASE_URL;
 
